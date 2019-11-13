92596917e2b1191a382c24bf60ae118d4de7dcee
Roberta Drayer <robertadrayer@gmail.com>
2019-11-12 22:14:05 -0800

    added forEach and event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,89 @@
-const url = 'https://randomuser.me/api/?results=12';
+//main URL + global variables
+const url = 'https://randomuser.me/api/?results=12&nat=us';
 const gallery = document.querySelector('.gallery');
-const card = document.getElementsByClassName('card');
-
-const modalContainer = document.createElement('div');
-modalContainer.className = 'modal-container';
-gallery.insertAdjacentElement('afterend', modalContainer);
-$('.modal-container').hide();
-
+const card = document.querySelector('.card');
+const body = document.querySelector('body');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-
-fetch(url)
+function fetchData(url) {
+    return fetch(url)
     .then(response => response.json())
-    .then(data => data.results.map(e => {
-        generateEmployeeData(e);
-        //generateModalCard(e);
-        //console.log(data.results)
-    }))
+}
 
+fetchData(url)
+    .then(data => generateEmployeeData(data.results))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
 function generateEmployeeData(data) {
-    const html = `
-        <div class="card">
+    data.map(e=> {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const html = `
             <div class="card-img-container">
-                <img class="card-img" src="${data.picture.medium}" alt="profile picture">
+                <img class="card-img" src="${e.picture.medium}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
-                <p class="card-text">${data.email}</p>
-                <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+                <h3 id="name" class="card-name cap">${e.name.first} ${e.name.last}</h3>
+                <p class="card-text">${e.email}</p>
+                <p class="card-text cap">${e.location.city}, ${e.location.state}</p>
             </div>
-        </div>
-        `;
-    gallery.innerHTML += html; 
+            `;
+
+        card.innerHTML = html;
+        gallery.appendChild(card);
+        
+        card.addEventListener('click', () => {
+            generateModalCard(e)
+        });
+    });
 }
 
-function generateModalCard(data) {
+function generateModalCard(e) {
+    const modalContainer = document.createElement('div');
+    modalContainer.className = 'modal-container';
+    $('.modal-container').hide();
+
+    //clean dob:
+    let dobYear = e.dob.date.slice(0,4);
+    let dobMonth = e.dob.date.slice(5,7);
+    let dobDay = e.dob.date.slice(8,10);
+    let dob =  `${dobMonth}/${dobDay}/${dobYear}`;
+
     const html = `  
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${data.picture.medium}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-                <p class="modal-text">${data.email}</p>
-                <p class="modal-text cap">${data.location.city}</p>
+                <img class="modal-img" src="${e.picture.medium}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${e.name.first} ${e.name.last}</h3>
+                <p class="modal-text">Email: ${e.email}</p>
                 <hr>
-                <p class="modal-text">${data.phone}</p>
-                <p class="modal-text">${data.location.street.number} ${data.location.street.name} ${data.location.state}, ${data.location.postcode}</p>
-                <p class="modal-text">Birthday: ${data.dob.date}</p>
+                <p class="modal-text">Phone: ${e.phone}</p>
+                <p class="modal-text">Address: ${e.location.street.number} ${e.location.street.name}</p>
+                <p class="modal-text">${e.location.city}, ${e.location.state}, ${e.location.postcode}</p>
+                <p class="modal-text">Birthday: ${dob} </p>
             </div>
         </div>
         `;
-    modalContainer.innerHTML += html;  
-} 
 
-// ------------------------------------------
-//  EVENT LISTENERS
-// ------------------------------------------
-
-
+    modalContainer.innerHTML = html;  
+    body.appendChild(modalContainer);
    
-
-
-// ------------------------------------------
-//  POST DATA
-// ------------------------------------------
-
-
-
+    modalContainer.addEventListener('click', () => {
+        $('.modal-container').hide();
+    });
+  
+    window.addEventListener('click', (e) => {
+        if (e.target === modalContainer){
+        $('.modal-container').hide();
+        }
+    });
+} 
 
 
 
